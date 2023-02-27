cb57cfd3e628a91696e0200d806bb5f0c3a80ff0
Kunoska <blirena@gmail.com>
2023-02-27 16:58:59 +0100

    Имплементиран dashboard template, креиран User CRUD, рути за user.blade.php, index.blade, create.blade.php, edit.blade.php and show.blade.php и имплементирано за countries migrate and seeder.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        $data = ["users" => $users];
+        return view('users.index')->with($data);
+    }
+    public function show ($id)
+    {
+        $user = User::FindOrFail($id);
+        $data = ['user' => $user];
+        return view('users.show')->with($data);
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name'   => 'required|max:20',
+            'email'  => 'email|unique:users,email',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('products/create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password']
+        ]);
+
+        return redirect(route('users.index'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::FindOrFail($id);
+        $data = ['user'=>$user];
+        return view('users.edit')->with('data');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::FindOrFail($id);
+        $user->fill($request->all())->save();
+        return redirect()->route('users.index');
+    }
+
+    public function destroy($id)
+    {
+        $user = User::FindOrFail($id);
+        $user->delete();
+        return redirect()->route('users.index');
+    }
+
+
+}
